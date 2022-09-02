b802ed0134dfad11461a48c0aa12c85051deb5c8
alt2qplz <alt2qplz@gmail.com>
2022-09-02 15:55:30 +0300

    cookie

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.all('/hello', (req, res, next) => {
-  console.log('HELLO!');
-  next();
-})
-
-const cb = (req, res, next) => {
-  console.log('CB');
-  next();
-}
-
-app.get('/hello', [cb, cb, cb, (req, res) => {
+app.get('/hello', (req, res) => {
+  res.cookie('token', 'asdfsa', {
+    domain: '',
+    path: '/',
+    secure: true,
+    expires: 600_000
+  })
+  // res.clearCookie('token')
   res.send('Hello!')
-}])
+})
 
 app.listen(port, () => {
   console.log(`server started on PORT: ${port}`)
