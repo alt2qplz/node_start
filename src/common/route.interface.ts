a09b605469fc551ab09f9e54be6337def637c6de
alt2qplz <alt2qplz@gmail.com>
2022-09-15 12:43:33 +0300

    middlewares for routes

--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction, Router } from 'express';
+import { IMiddleware } from './middleware.interface';
 
 export interface IControllerRoute {
   path: string;
   method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
-
   func(req: Request, res: Response, next: NextFunction): void;
+  middlewares?: IMiddleware[];
 }
 
 export type ExpressReturnType = Response<any, Record<string, any>>;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,7 +20,7 @@ export class UsersController extends BaseController implements IUserController{
 
     this.bindRoutes([
       { path: '/login', method: 'post', func: this.login },
-      { path: '/register', method: 'post', func: this.register }
+      { path: '/register', method: 'post', func: this.register, middlewares: [] }
     ]);
   }
 
